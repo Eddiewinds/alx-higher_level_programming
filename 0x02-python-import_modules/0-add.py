c799f6f56b7a9496b8afa9242bc330ccba289ed9
Eddiewinds <ediomoabasiemmanuel@gmail.com>
2024-09-12 15:28:20 -0700

    Here We Go! 0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-from add_0.py import add
+from add_0 import add
 
 if __name__ == "__main__":
 
     a = 1
     b = 2
 
-    result = add(a, b)
-    
-    print("{} + {} = {}".format(a, b, result))
+    answer = add(a, b)
+
+    print("{} + {} = {}".format(a, b, answer))
